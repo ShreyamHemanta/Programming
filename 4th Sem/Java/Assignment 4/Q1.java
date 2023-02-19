cb7be6d9f7c35e633682cbe676d8762c825c6481
Shreyam Hemanta <shreyam2014@gmail.com>
2023-02-19 22:03:07 +0530

    initial commit

---
+++ 4th Sem/Java/Assignment 4/Q1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Q1
+{
+    public static void main(String[] args)
+	{
+	    System.out.println("Vaidehi Gupta");
+		System.out.println("2105214");
+		System.out.println("CSE-28");
+		System.out.println("Computer Science and Engineering");
+	}
+}
