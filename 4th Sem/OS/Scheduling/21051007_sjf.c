1507c4bda85480605339bc35618e2b29eafce8d6
Shreyam Hemanta <shreyam2014@gmail.com>
2023-03-30 22:08:28 +0530

    commit

---
+++ 4th Sem/OS/Scheduling/21051007_sjf.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+struct process {
+	int p_1007; //process Number
+	int at_1007; //Arrival time
+	int bt_1007; //Burst Time
+};
+
+int main() {
+	int n,ttat_1007=0,twt_1007=0;
+	float avg_tat,avg_wt;
+	printf("Enter number of processes ");	
+	scanf("%d",&n);
+	struct process p[n];
+	
+	//input
+	for(int i=0;i<n;i++) {
+		printf("Enter process id ");
+		scanf("%d",&p[i].p_1007);
+		printf("Enter arrival time ");
+		scanf("%d",&p[i].at_1007);
+		printf("Enter burst time ");
+		scanf("%d",&p[i].bt_1007);
+	}
+	
+	//sorting using burst time
+	for(int i=0;i<n-1;i++) {
+		for(int j=i+1;j<n;j++) {
+			if(p[i].bt_1007>p[j].bt_1007) {
+				struct process temp = p[i];
+				p[i] = p[j];
+				p[j] = temp;
+			}
+		}
+	}
+	
+	//calculation of turn around time and completion time
+	for(int i=0, ct_1007=p[0].at_1007;i<n;i++) {
+		printf("P%d",p[i].p_1007);
+		twt_1007 += ct_1007 - p[i].at_1007;
+		ct_1007 += p[i].bt_1007;
+		ttat_1007 += ct_1007 - p[i].at_1007;
+	}
+	
+	avg_tat =((float)ttat_1007/n)-1;
+	avg_wt =((float)twt_1007/n)-1;
+	
+	printf("\nThe average turn around time is %.2f",avg_tat);
+	printf("\nThe average waiting time is %.2f\n",avg_wt);
+	
+	return 0;
+}
+	
